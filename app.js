8e5d05f76e389fd421a3c0babfdd3297786da7cf
BHardelin <baptiste.hardelin@adeo.com>
2022-12-12 20:40:36 +0100

    Can add a thing and get them all !

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-import express from "express";
-import mongoose from "mongoose";
-import * as dotenv from "dotenv";
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const Thing = require("./models/Thing");
 
 dotenv.config();
 
@@ -28,33 +30,25 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({ message: "Objet créé !" });
+app.post("/api/stuff", async (req, res, next) => {
+  delete req.body._id;
+
+  const thing = new Thing({
+    ...req.body,
+  });
+
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré ! " }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 app.get("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) =>
+      res.status(404).json({ message: "no ressource found!", error })
+    );
 });
 
-export default app;
+module.exports = app;
